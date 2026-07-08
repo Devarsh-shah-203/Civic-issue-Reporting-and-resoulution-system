1d84b6d2d9c674836eb729ad4df39ef5c11cca2a
Devarsh-shah-203 <devarshshah203@gmail.com>
2026-07-08 16:52:01 +0530

    upload

---
+++ backend/src/middleware/upload.middleware.js
@@ -0,0 +1,62 @@
+import multer from "multer";
+
+import path from "path";
+
+import ApiError from "../utils/ApiError.js";
+
+const storage = multer.diskStorage({
+
+    destination(req, file, cb) {
+
+        cb(null, "uploads/");
+    },
+
+    filename(req, file, cb) {
+
+        cb(
+            null,
+            Date.now() +
+                "-" +
+                Math.round(Math.random() * 1e9) +
+                path.extname(file.originalname)
+        );
+
+    },
+
+});
+
+const fileFilter = (req, file, cb) => {
+
+    if (file.mimetype.startsWith("image/")) {
+
+        cb(null, true);
+
+    } else {
+
+        cb(
+            new ApiError(
+                400,
+                "Only image files are allowed."
+            ),
+            false
+        );
+
+    }
+
+};
+
+const upload = multer({
+
+    storage,
+
+    fileFilter,
+
+    limits: {
+
+        fileSize: 5 * 1024 * 1024,
+
+    },
+
+});
+
+export default upload;
