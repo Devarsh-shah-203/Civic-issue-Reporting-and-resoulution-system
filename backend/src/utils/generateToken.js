8f58dd1b3baee12ebe796cee3340ed48d5cd1f55
Devarsh-shah-203 <devarshshah203@gmail.com>
2026-07-08 16:25:27 +0530

    added email payload

--- backend/src/utils/generateToken.js
+++ backend/src/utils/generateToken.js
@@ -1,8 +1,8 @@
 import jwt from "jsonwebtoken";
 
-export const generateAccessToken = (id) => {
+export const generateAccessToken = (id,email) => {
   return jwt.sign(
-    { id },
+    { id,email },
     process.env.ACCESS_TOKEN_SECRET,
     {
       expiresIn: "15m",
